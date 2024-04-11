eff8a77a694cc86ec1c4f16b36712b5d4f88863e
Shravani K <shravani.qualtech@gmail.com>
2024-04-11 14:13:44 +0530

    TaskId:lDEMCxBX created changes

--- evento/src/modules/login/components/OtpErrorModal.jsx
+++ evento/src/modules/login/components/OtpErrorModal.jsx
@@ -9,27 +9,25 @@ import {
 } from "./atoms";
 
 const OtpErrorModal = ({ modalOpen, handleCloseModal, modalMessage }) => {
-  return (
-    modalOpen && (
-      <StyledMainErrorBox style={{ marginTop: "-230px" }}>
-        <StyledSubErrorBox>
-          <img src={danger} alt="Danger Icon" />
-          <StyleErrorTypography variant="body1">
-            <p> {modalMessage}</p>
-          </StyleErrorTypography>
-        </StyledSubErrorBox>
+  modalOpen && (
+    <StyledMainErrorBox style={{ marginTop: "-230px" }}>
+      <StyledSubErrorBox>
+        <img src={danger} alt="Danger Icon" />
+        <StyleErrorTypography variant="body1">
+          <p> {modalMessage}</p>
+        </StyleErrorTypography>
+      </StyledSubErrorBox>
 
-        <StyledModalButton onClick={handleCloseModal} sx={{ mt: 2 }}>
-          <img src={OkBtn} alt="Close Button" />
-        </StyledModalButton>
+      <StyledModalButton onClick={handleCloseModal} sx={{ mt: 2 }}>
+        <img src={OkBtn} alt="Close Button" />
+      </StyledModalButton>
 
-        <StyledInvalidText>
-          <hr />
-          <h4>Oops!</h4>
-          <p>Enter the correct email address and password to log in</p>
-        </StyledInvalidText>
-      </StyledMainErrorBox>
-    )
+      <StyledInvalidText>
+        <hr />
+        <h4>Oops!</h4>
+        <p>Enter the correct email address and password to log in</p>
+      </StyledInvalidText>
+    </StyledMainErrorBox>
   );
 };
 
--- evento/src/modules/login/components/OtpSuccessModal.jsx
+++ evento/src/modules/login/components/OtpSuccessModal.jsx
@@ -8,22 +8,20 @@ import {
 } from "./atoms";
 
 const OtpSuccessModal = ({ modalOpen, handleCloseModal, modalMessage }) => {
-  return (
-    modalOpen && (
-      <StyledMainBox style={{ marginTop: "-230px" }}>
-        <StyledRedModalButton onClick={handleCloseModal}>
-          <img src={OkBtn} alt="Close Button" />
-        </StyledRedModalButton>
-        <StyledSubSuccessBox>
-          <img src={success} />
-          <h2>{modalMessage}</h2>
-          <p>
-            Your OTP is successfully verify You can close this window and
-            continue using the product.
-          </p>
-        </StyledSubSuccessBox>
-      </StyledMainBox>
-    )
+  modalOpen && (
+    <StyledMainBox style={{ marginTop: "-230px" }}>
+      <StyledRedModalButton onClick={handleCloseModal}>
+        <img src={OkBtn} alt="Close Button" />
+      </StyledRedModalButton>
+      <StyledSubSuccessBox>
+        <img src={success} />
+        <h2>{modalMessage}</h2>
+        <p>
+          Your OTP is successfully verify You can close this window and continue
+          using the product.
+        </p>
+      </StyledSubSuccessBox>
+    </StyledMainBox>
   );
 };
 
